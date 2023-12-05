c249f77446bfdcb6b847973c88bf0a183235bf46
soul <142450473+centidemo@users.noreply.github.com>
2023-12-04 20:59:58 -0500

    update bento.py
    code is reading json file correctly and returning table for tag

--- bento.py
+++ bento.py
@@ -1,29 +1,13 @@
 import json
 import pandas
 
-def load_data():
-    # load subbox data from JSON file
-    with open("subbox_data.json") as f:
-        subbox_data = json.loads(f.read())
-
-    # create items, store them in list
-    subboxes_in_db = []
-    for subbox in subbox_data:
-        tag, title, ingredients = (
-            subbox["tag"],
-            subbox["title"],
-            subbox["ingredients"],
-        )
-
-        db_subbox = subbox.create_subbox(tag, title, ingredients)
-        subboxes_in_db.append(db_subbox)
-load_data()
-
 categories = ["carb", "protein", "fruit", "raw veggie", "itamemono", "tsukemono", "agemono", "yakimono", "norimaki"]
-def options():
-    subbox.query.get(tag)
+
+data = pandas.read_json("subbox_data.json", orient="records")
+
+def options(tag):
+    return data[data.tag==tag]
 # return every item name in json list with corresponding tag
-info = # return rest of information from item name in list
 
 count = int(input ("Hello! To begin creating your bento enter how many containers are in your box: "))
 # enter number of containers
@@ -40,12 +24,11 @@ while count > 0:
         # search list for tag and print results
         
         response = input("Enter option for more information or 'go back'.\n")
-        while response != "return":
-            print(info(response))
-            response = input("Enter 'return' to go back:")
+       # while response != "return":
+          #  print(info(response))
+            #response = input("Enter 'return' to go back:")
     
     selection = input("Enter selection for container",count)
     containers.insert(index, selection)
     # selection is inserted into list at the current box 
     count -= 1
-  
